6780c8a88e71db162447a9c6cd5018f7f4eddb19
ali <aliidolati@gmail.com>
2023-02-08 18:18:29 +0330

    warning dont open the project!!!!

--- src/main/java/Mainclass.java
+++ src/main/java/Mainclass.java
@@ -3,6 +3,7 @@ public class Mainclass {
     public static void main(String[] args) {
         System.out.println("salam piaz darin :)");
         System.out.println("sibzamini chi?");
+        System.out.println("a message from venus");
     }
 
 }
